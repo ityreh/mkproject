a0e13c6565541ff88ac1fb9295313ee04b33cbfa
Yannick Rehberger <yr@ityreh.de>
2021-01-12 22:46:26 +0100

    Reorder server files.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import (Flask, abort, flash, redirect, render_template, request,
+                   session)
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "index"
 
+
 @app.route("/make")
 def make():
     return "Make a readme."
+
 
 @app.route("/make/<string:name>/")
 def makeSomething(name):
     return render_template('index.html', name=name)
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
